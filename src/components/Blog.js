4dcde637f384ec1d6f0c2622bb2e38fa0345dc32
Jussi <jussi.kanto@tuni.fi>
2022-09-23 19:04:55 +0300

    Add 5.7 - blogilistan frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog, buttonLabel }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleClickBlogView = () => {
+    setVisible(!visible);
+  };
+
+  console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        {!visible ? (
+          <button onClick={handleClickBlogView}>view</button>
+        ) : (
+          <>
+            <button onClick={handleClickBlogView}>hide</button>
+            <br />
+            {blog.url}
+            <br />
+            likes {blog.likes} <button>like</button>
+            <br />
+            {blog.user.name}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
