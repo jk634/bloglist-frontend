bac78a4564f01e186bc9dcb0c613ddc1d50c83b3
Jussi <jussi.kanto@tuni.fi>
2022-09-29 16:20:04 +0300

    Add 5.15 - blogilistan testit, step3

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
+import { testBlog, testUser } from '../utils/testHelper';
 
 describe('Blog', () => {
   test('renders title and author by default', () => {
@@ -21,21 +22,7 @@ describe('Blog', () => {
   });
 
   test('renders title, author, url and likes when show button is clicked', async () => {
-    const blog = {
-      title: 'should render title, author, url and likes',
-      author: 'Timo Testaaja',
-      url: 'www.test.com',
-      likes: 0,
-      user: {
-        name: 'Sami Test',
-      },
-    };
-
-    const testUser = {
-      name: 'Sami Test',
-    };
-
-    const renderedBlog = render(<Blog blog={blog} user={testUser} />);
+    const renderedBlog = render(<Blog blog={testBlog} user={testUser} />);
     const div = renderedBlog.container.querySelector('.blog');
 
     const user = userEvent.setup();
@@ -45,5 +32,21 @@ describe('Blog', () => {
     expect(div).toHaveTextContent('should render title, author, url and likes');
     expect(div).toHaveTextContent('www.test.com');
     expect(div).toHaveTextContent('likes');
+  });
+
+  test('verify that the call has occurred twice when the click button is clicked twice', async () => {
+    const likeMock = jest.fn();
+
+    render(<Blog blog={testBlog} user={testUser} modifyBlog={likeMock} />);
+
+    const user = userEvent.setup();
+    const showAll = screen.getByText('view');
+    await user.click(showAll);
+
+    const likeButton = screen.getByText('like');
+    await user.click(likeButton);
+    await user.click(likeButton);
+
+    expect(likeMock.mock.calls).toHaveLength(2);
   });
 });
