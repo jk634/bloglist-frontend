331a99ef065261d392c22e30f951420ebc6594d4
Jussi <jussi.kanto@tuni.fi>
2022-09-29 15:28:22 +0300

    Add 5.14 - blogilistan testit, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 describe('Blog', () => {
@@ -17,5 +18,32 @@ describe('Blog', () => {
     expect(div).toHaveTextContent('should render title and Timo Testaaja');
     expect(div).not.toHaveTextContent('www.test.com');
     expect(div).not.toHaveValue(0);
+  });
+
+  test('renders title, author, url and likes when show button is clicked', async () => {
+    const blog = {
+      title: 'should render title, author, url and likes',
+      author: 'Timo Testaaja',
+      url: 'www.test.com',
+      likes: 0,
+      user: {
+        name: 'Sami Test',
+      },
+    };
+
+    const testUser = {
+      name: 'Sami Test',
+    };
+
+    const renderedBlog = render(<Blog blog={blog} user={testUser} />);
+    const div = renderedBlog.container.querySelector('.blog');
+
+    const user = userEvent.setup();
+    const button = renderedBlog.getByText('view');
+    await user.click(button);
+
+    expect(div).toHaveTextContent('should render title, author, url and likes');
+    expect(div).toHaveTextContent('www.test.com');
+    expect(div).toHaveTextContent('likes');
   });
 });
